6d705985004ac8ef2a1ba6816b2f9f9b8c1fd1a2
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-09 14:58:57 +0300

    Added component for rendering

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { About, Header, Footer, Skills, Testimonials, Work } from './container/index';
+import { Navbar } from './components';
+
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className = "app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
   )
 }
 
