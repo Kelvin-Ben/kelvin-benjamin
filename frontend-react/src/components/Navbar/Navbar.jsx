6dde4bdd566f4ed11e0c4ca5424a39c32e4a14e0
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-10 02:55:34 +0300

    Add navbar elements to be displayed

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
 
+import { images } from '../../constants';
 import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="A logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'contact', 'work', 'skills', 'testimonials'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
