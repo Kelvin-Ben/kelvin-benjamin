dad77b6ceb226a9737dc3d3176cdf2af956a1af0
Kelvin-Ben <kevoben14@gmail.com>
2023-02-26 23:32:13 +0300

    Create database schemas

--- backend_sanity/sanity.config.js
+++ backend_sanity/sanity.config.js
@@ -2,20 +2,27 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-import testimonials from './schemas/testimonial'
+import works from './schemas/works'
+import testimonials from './schemas/testimonials'
+import brands from './schemas/brands'
+import abouts from './schemas/abouts'
+import experiences from './schemas/experiences'
+import skills from './schemas/skills'
+import workExperience from './schemas/workExperience'
+import contact from './schemas/contact'
 
 export default defineConfig({
   name: 'default',
   title: 'Portfolio',
 
-  projectId: 'lggxlpb3',
-  dataset: 'portfolio',
+  projectId: 'kdseu2od',
+  dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes.concat([
-      testimonials
+      testimonials, abouts, brands, contact, experiences, skills, workExperience, works
     ])
   },
 })
--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -3,14 +3,14 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import works from './works'
-import testimonials from './testimonials'
-import brands from './brands'
 import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
 import experiences from './experiences'
 import skills from './skills'
+import testimonials from './testimonials'
 import workExperience from './workExperience'
-import contact from './contact'
+import works from './works'
 
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -19,7 +19,8 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([works, testimonials, brands, abouts, skills, workExperience, experiences, contact
+  types: schemaTypes.concat([
     /* Your types here! */
+    works, testimonials, brands, abouts, skills, workExperience, experiences, contact
   ]),
 })
