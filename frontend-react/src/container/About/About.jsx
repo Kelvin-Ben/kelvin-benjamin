80093bf8a3bb6dbea9c4aee2927c6bf4ccf0e48c
Kelvin-Ben <kevoben14@gmail.com>
2023-03-03 10:54:05 +0300

    Create state function to add data dynamically

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
+import { urlFor, client  } from '../../client';
 
 import { images } from "../../constants";
 import './About.scss';
 
-const abouts = [
-  { title: 'Web Developer', description: 'I am a good web developer.', imgUrl: images.about01 },
-  { title: 'Web Designer', description: 'I am a good web developer.', imgUrl: images.about02 },
-  { title: 'Web UI/UX Designer', description: 'I am a good web developer.', imgUrl: images.about03 },
-  { title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04 }
 
-];
-const About = () => (
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data))
+    console.log(About);
+  }, []);
+  return (
   <>
     <h2 className="head-text">I know that <span>Good Development</span><br /> means <span>Good Business</span>
     </h2>
@@ -24,13 +28,14 @@ const About = () => (
           className="app__profile-item"
           key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20}}> {about.title} </h2>
             <p className="p-text" style={{ marginTop: 10}}> {about.description} </p>
           </motion.div>
       ))}
     </div>
   </>
-);
+  );
+      };
 
 export default About;
--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -15,7 +16,7 @@ const scaleVariants = {
   },
 };
 const Header = () => (
-  <div id="home" className="app__header app__flex">
+  <div className="app__header app__flex">
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
@@ -63,4 +64,4 @@ const Header = () => (
   </div>
 );
 
-export default Header;
+export default AppWrap(Header, 'home');
