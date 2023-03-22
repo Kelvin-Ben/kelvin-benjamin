c6715d1de6c7724d7ab2c9d37b98d9a480b94bcd
Kelvin-Ben <kevoben14@gmail.com>
2023-03-22 03:18:45 +0300

    Modified the navigations dots to match current page

--- frontend-react/src/components/NavigationDots.jsx
+++ frontend-react/src/components/NavigationDots.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const NavigationDots = ( { active }) => {
   return (
     <div className="app__navigation">
-      {['home', 'about', 'work', 'skills', 'testimonials', 'contact',].map(
+      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
         (item, index) => (
             <a 
             href={`#${item}`}
