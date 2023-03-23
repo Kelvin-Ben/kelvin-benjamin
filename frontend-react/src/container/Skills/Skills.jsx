dddb0a6380af0eef3c89cec2f5627b048d5ae133
Kelvin-Ben <kevoben14@gmail.com>
2023-03-23 15:59:21 +0300

    Create experience section

--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { animate, motion} from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+// import ReactTooltip from 'react-tooltip';
+import { Tooltip } from 'react-tooltip';
 import { AppWrap } from '../../wrapper';
 import { urlFor, client} from '../../client';
 
@@ -44,9 +45,50 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+        
+        {/* experience section */}
+        <motion.div className='app__skills-exp'>
+          {experience.map((experience) => (
+            <motion.div
+              className='app__skills-exp-item'
+              key={experience.year}
+            >
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{experience.year}</p>
+              </div>
+              <motion.div className='app__skills-exp-works'>
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      // key={work.name}
+                      key={`${experience.year}-${work.name}`}
+                    >
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+                    <Tooltip
+                      // id={work.name + '-tooltip'}
+                      id={`${work.name}-${experience.year}-tooltip`}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    > 
+                      {work.desc}
+                    </Tooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+            ))}
+         </motion.div>
       </div>
     </>
   )
 };
 
-export default Skills;
+export default AppWrap(Skills, 'skills');
