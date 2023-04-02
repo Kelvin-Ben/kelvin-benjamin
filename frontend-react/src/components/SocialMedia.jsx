7bf8ea173dfa5e1aa8ff34f71adbcf28dc6813e2
Kelvin-Ben <kevoben14@gmail.com>
2023-04-02 13:12:24 +0300

    Add a link to medium account

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import { BsTwitter, BsInstagram, BsFacebook } from 'react-icons/bs';
+import { BsTwitter, BsInstagram, BsFacebook, BsLinkedin, BsMedium } from 'react-icons/bs';
 import { FaFacebook } from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className= "app__social">
       <div>
+        <a href='https://twitter.com/kevoyout144' target="_blank">
         <BsTwitter />
+        </a>
       </div>
       <div>
-        <BsInstagram />
+        <a href='https://www.instagram.com/kevoben14/' target="_blank">
+          <BsInstagram />
+        </a>
       </div>
       <div>
-        <BsFacebook />
+        <a href='https://www.facebook.com/profile.php?id=100006344356184' target="_blank">
+          <BsFacebook />
+        </a>
+      </div>
+      <div>
+        <a href='https://www.linkedin.com/in/kelvinben/' target="_blank">
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href='https://medium.com/@kevoyout144' target="_blank">
+          <BsMedium />
+        </a>
       </div>
     </div>
   )
